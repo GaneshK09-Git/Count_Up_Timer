84746bf824e973559e80d6411169779c8072ce92
Ganesh Kumar <ganeshkumargana1@gmail.com>
2025-08-10 10:25:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#count-up timer
+
+
+import time
+
+def count(end, start=0): #Default arguments should be placed at last and no default should be placed before default arguments.
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("Done!")
+
+count(20, 9) #suppose say (a,b) then countdown starts from b and ends at a 
